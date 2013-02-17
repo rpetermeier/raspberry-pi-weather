f8fc5bb84280a5ada6b37f4d2696a0fe9949c6ed
Robert Petermeier <robert.petermeier@gmx.de>
2013-02-17 17:06:25 +0100

    Changed the mock implementation of the meter

--- src/meter.rb
+++ src/meter.rb
@@ -2,7 +2,6 @@ class Meter
   
   def read
     `/home/pi/software/SHT21/Raspi-SHT21-V3_0_0/sht21 S`.chomp
-    # `ruby -v`.chomp
   end
   
 end
--- src/mock-meter.rb
+++ src/mock-meter.rb
@@ -1,7 +1,8 @@
 class MockMeter
   
   def read
-    "18.9\t36"
+    # "18.9\t36"
+    `ruby -v`.chomp
   end
   
 end
